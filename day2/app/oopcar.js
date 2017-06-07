db91c506d6cfb963c154ccbfa35859f72a570b93
umeononihu adaobi <adaumeh2003@yahoo.com>
2017-06-07 16:10:02 +0100

    my car oob submitted

---
+++ day2/app/oopcar.js
@@ -0,0 +1,43 @@
+
+
+class Car{
+	constructor(name, model, type){
+	
+	
+	this. name  = name;
+	this.model = model;
+	this.type = type;
+	this.numofWheels= 4;
+	this.numofdoors = 4;
+
+	if(this.name  === undefined && this.model === undefined){
+		 this.name   = 'General';
+		 this.model  = 'GM'
+	}else{
+		this.name = name;
+	}
+	 if(this.name === 'Porshe' || this.name === 'Koenigsegg'){
+		this.numOfDoors = 2;
+	}else{
+		this.numOfDoors = 4;
+	}
+
+	if(type === 'trailer'){
+		this.numOfWheels = 8;
+		this.speed = '0 km/h';
+		this.isSaloon = false;
+	} else {
+		this.numOfWheels = 4;
+		this.isSaloon = true;
+	}
+	
+}
+
+}
+
+Car.prototype.drive = function(speed) {
+ if(speed === 7){this.speed = '77 km/h';}
+  else if(speed === 5){this.speed = '250 km/h';}
+  return this;
+}
+module.exports= Car;
